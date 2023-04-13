f5f65e617989400be8f2f86f2325abb78b41b6c5
Abul Hasan <abulhasan.swe@gmail.com>
2023-04-13 15:15:09 -0700

    modified schema for service provider

--- back-end/model/serviceProvider.js
+++ back-end/model/serviceProvider.js
@@ -1,5 +1,6 @@
-module.exports =  (Schema) =>
-   ({ name: {
+module.exports = (Schema) =>
+({
+    name: {
         type: String,
     },
     email: {
@@ -19,7 +20,12 @@ module.exports =  (Schema) =>
         type: String,
     },
     workingHours: {
-        type: String,
+        open: {
+            type: String,
+        },
+        close: {
+            type: String
+        }
     },
     avgRating: {
         type: Number,
@@ -38,8 +44,12 @@ module.exports =  (Schema) =>
     //todo tuple
     services: [{
         type: Schema.Types.ObjectId, ref: 'Services',
+        rate: {
+            type: Number,
+            default: 0
+        }
     }],
     address: {
         type: Schema.Types.ObjectId, ref: 'Address',
-      },
+    },
 });
