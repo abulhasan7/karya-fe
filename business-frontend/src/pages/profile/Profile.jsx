9cf45f7c7920c3a75291d7ddaf89fe786c6fba32
Tuba Ahmed <md.tuba.ahmed@gmail.com>
2023-05-07 15:35:08 -0700

    New actions buttons on BusinessProfile

--- business-frontend/src/pages/profile/Profile.jsx
+++ business-frontend/src/pages/profile/Profile.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Avatar, IconButton } from '@mui/material';
 import ModeEditOutlineTwoToneIcon from '@mui/icons-material/ModeEditOutlineTwoTone';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import { Divider } from '@mui/material';
 import { useSelector } from 'react-redux';
 // import BACKEND_URL from '../../config/configBackend';
 
@@ -15,29 +20,65 @@ export default function Profile() {
 		<div>
 			<MenuBar />
 			<div className="business-profile-container">
-				<div className="business-profile-header">
-					<div className="business-profile-image">
-						<Avatar
-							src={''}
-							sx={{
-								width: 140,
-								height: 140,
-							}}
-						/>
+				<div>
+					<div className="business-profile-header">
+						<div className="business-profile-image">
+							<Avatar
+								src={profile.primaryImage}
+								sx={{
+									width: 140,
+									height: 140,
+								}}
+							/>
+						</div>
+						<div className="business-profile-name">
+							<span>{profile.name}</span>
+							<IconButton
+								aria-label="edit"
+								onClick={() => {
+									window.location.assign(`/profile-edit/`);
+								}}
+							>
+								<ModeEditOutlineTwoToneIcon />
+							</IconButton>
+						</div>
 					</div>
-					<div className="business-profile-name">
-						<span>{profile.name}</span>
-						<IconButton
-							aria-label="edit"
-							onClick={() => {
-								window.location.assign(`/profile-edit/`);
-							}}
-						>
-							<ModeEditOutlineTwoToneIcon />
-						</IconButton>
-					</div>
+					<ActiveJobsList />
 				</div>
-				<ActiveJobsList />
+				<div className="bo-side-card">
+					<Paper
+						sx={{
+							width: '300px',
+							padding: '15px',
+						}}
+					>
+						<Button
+							variant="contained"
+							size="small"
+							sx={{
+								textTransform: 'unset',
+								width: '100%',
+								mb: '10px',
+							}}
+							endIcon={<AccessTimeIcon />}
+						>
+							Edit Hours
+						</Button>
+						<Divider />
+						<Button
+							variant="contained"
+							size="small"
+							sx={{
+								textTransform: 'unset',
+								width: '100%',
+								mt: '10px',
+							}}
+							endIcon={<DesignServicesIcon />}
+						>
+							Edit Services
+						</Button>
+					</Paper>
+				</div>
 			</div>
 		</div>
 	);
